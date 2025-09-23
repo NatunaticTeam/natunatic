f60dcc94a64707eff088ef77a11338461a80618c
n4c <heyimdaki06@gmail.com>
2025-09-23 13:15:18 -0300

    something

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import './style.css'
+import './styles/team.css'
 import n4cPfp from './assets/n4c.jpg'
 import julibrosPfp from './assets/julibros.jpg'
 import aaronPfp from './assets/aaron.jpg'
@@ -47,60 +48,60 @@ document.querySelector<HTMLDivElement>('#main')!.innerHTML = `
         </ul>
       </div>
     </div>
-    <div id="content">
+    <div id="content"></div>
   </div>
 `;
 
 document.querySelector<HTMLDivElement>('#content')!.innerHTML = `
   <h1>welcome to natunatic!</h1>
-      <h5>page is still a very big wip but we finally decided to keep this design bc julibros really liked it</h5>
-      <div class="spacing"></div>
-      <div id="team">
-        <div class="small-spacing"></div>
-        <div class="small-spacing"></div>
-        <h2>of course we present ourselves appropiately</h2>
-        <h3>team is composed of mainly 3 persons and their friends, and possibly contributors from many sources</h3>
-        <h5 id="something1">the cards are clickables and the descriptions are written from our own perspectives btw</h5>
-        <div class="small-spacing"></div>
-        <div id="members">
-          <div class="person" id="julibros" onclick="window.location.href='https://www.youtube.com/@Julibros'">
-            <img src="https://yt3.googleusercontent.com/7JKyQh4oumdOajuJWY0ADyCu_nPZfkrzZs7fusHPV2-zkCJ6mpP7u-vFxBXMemwS7xSwnYKt=s160-c-k-c0x00ffffff-no-rj" alt="n4c" class="pfp"/>
-            <p></p>
-            <span class="member-name">Julibros</span>
-            <p></p>
-            <span class="member-roles">sprites, music and some animations</span>
-            <p></p>
-            <span class="member-desc">
-              lazy guy doesnt wanna do his desc
-            </span>
-          </div>
-          <div class="person" id="n4c" onclick="window.location.href='https://www.youtube.com/@n4c-n'">
-            <img src="https://yt3.googleusercontent.com/1qE7CfOk4pRJMXAqBy8BaVWeUDL3Z__vhtyhoEyX80E_qUHt5oOD0VwS8R3b1f1vmB6q3Vkz=s160-c-k-c0x00ffffff-no-rj" alt="n4c" class="pfp"/>
-            <p></p>
-            <span class="member-name">n4c</span>
-            <p></p>
-            <span class="member-roles">lead programmer, lazy ass animator and sfx maker</span>
-            <p></p>
-            <span class="member-desc">
-              "could say im a chill guy, very good at work but lazy, im a fat###,
-              i love cats (specially milo, the one on my pfp) and i like rpgs and fighting games in general,
-              i like anything that my ears like and im usually very introverted and unsociable,
-              if i like you then im sticking around you as much as i can
-              and i dont really like sports but i enjoy basketball,
-              im a project moon enthusiast and poser (yes im admiting it i dont get whats wrong) and i like photography,
-              thats pretty much it for me,
-              i could add something more but i will run out of space or it will be too empty so im not trying for now
-              (i did it didnt i? doesnt matter, its not like i didnt want to lol)"
-            </span>
-          </div>
-          <div class="person" id="aaron" onclick="window.location.href='https://www.youtube.com/@Elmisterio123xd'">
-            <img src="https://yt3.googleusercontent.com/MwMib2f-QoKLiguzYLLDqVgqU9pgTBcYY43nSJLepSldVe9rrt3e6lmhHSb3fDCGkYWJOV4q8A=s160-c-k-c0x00ffffff-no-rj" alt="n4c" class="pfp"/>
-            <p></p>
-            <span class="member-name">Elmisterio123x</span>
-            <p></p>
-            <span class="member-roles">frontend programmer (not yet), animator, sprites</span>
-            <p></p>
-            <span class="member-desc">
+  <h5>page is still a very big wip but we finally decided to keep this design bc julibros really liked it</h5>
+  <div class="spacing"></div>
+    <div id="team">
+      <div class="small-spacing"></div>
+      <div class="small-spacing"></div>
+      <h2>of course we present ourselves appropiately</h2>
+      <h3>team is composed of mainly 3 persons and their friends, and possibly contributors from many sources</h3>
+      <h5 id="something1">the cards are clickables and the descriptions are written from our own perspectives btw</h5>
+      <div class="small-spacing"></div>
+      <div id="members">
+        <div class="person" id="julibros" onclick="window.location.href='https://www.youtube.com/@Julibros'">
+          <img src="https://yt3.googleusercontent.com/7JKyQh4oumdOajuJWY0ADyCu_nPZfkrzZs7fusHPV2-zkCJ6mpP7u-vFxBXMemwS7xSwnYKt=s160-c-k-c0x00ffffff-no-rj" alt="n4c" class="pfp"/>
+          <p></p>
+          <span class="member-name">Julibros</span>
+          <p></p>
+          <span class="member-roles">sprites, music and some animations</span>
+          <p></p>
+          <span class="member-desc">
+            lazy guy doesnt wanna do his desc
+          </span>
+        </div>
+        <div class="person" id="n4c" onclick="window.location.href='https://www.youtube.com/@n4c-n'">
+          <img src="https://yt3.googleusercontent.com/1qE7CfOk4pRJMXAqBy8BaVWeUDL3Z__vhtyhoEyX80E_qUHt5oOD0VwS8R3b1f1vmB6q3Vkz=s160-c-k-c0x00ffffff-no-rj" alt="n4c" class="pfp"/>
+          <p></p>
+          <span class="member-name">n4c</span>
+          <p></p>
+          <span class="member-roles">lead programmer, lazy ass animator and sfx maker</span>
+          <p></p>
+          <span class="member-desc">
+            "could say im a chill guy, very good at work but lazy, im a fat###,
+            i love cats (specially milo, the one on my pfp) and i like rpgs and fighting games in general,
+            i like anything that my ears like and im usually very introverted and unsociable,
+            if i like you then im sticking around you as much as i can
+            and i dont really like sports but i enjoy basketball,
+            im a project moon enthusiast and poser (yes im admiting it i dont get whats wrong) and i like photography,
+            thats pretty much it for me,
+            i could add something more but i will run out of space or it will be too empty so im not trying for now
+            (i did it didnt i? doesnt matter, its not like i didnt want to lol)"
+          </span>
+        </div>
+        <div class="person" id="aaron" onclick="window.location.href='https://www.youtube.com/@Elmisterio123xd'">
+          <img src="https://yt3.googleusercontent.com/MwMib2f-QoKLiguzYLLDqVgqU9pgTBcYY43nSJLepSldVe9rrt3e6lmhHSb3fDCGkYWJOV4q8A=s160-c-k-c0x00ffffff-no-rj" alt="n4c" class="pfp"/>
+          <p></p>
+          <span class="member-name">Elmisterio123x</span>
+          <p></p>
+          <span class="member-roles">frontend programmer (not yet), animator, sprites</span>
+          <p></p>
+          <span class="member-desc">
             "im a sonic fan, i like drawing
             and i help people when i can without expecting anything in exchange,
             i like exotic[butters] animals like hedgehogs and bunnies and
@@ -112,11 +113,32 @@ document.querySelector<HTMLDivElement>('#content')!.innerHTML = `
             pretty cool guy but kinda dumb sometimes, but does lots of cool things,
             we tryna get him to be a composer and producer but doesnt want too
             because he doesnt know how to supposedly lol
-            </span>
-            </div>
+          </span>
+          </div>
         </div>
-        <div id="pequeñoagradecimientojeje">
-          <h6>gracias jean por revisar esta cosa fea <3</h6>
+        <div id="special-thanks">
+          <h3>special thanks to people that didnt necessarily collaborate or something, but we appreciate:</h3>
+          <div id="special-thanks-persons">
+            <div class="small-person">
+              <span class="member-name">jean</span>
+              <p></p>
+              <span class="member-desc">
+                jeany beany
+                <div></div>
+                umm yeah he cool, complimented the thing above this
+                <div></div>
+                love you twin -n4c
+              </span>
+            </div>
+            <div class="small-person hidden">
+              <span class="member-name">someone</span>
+              <p></p>
+              <span class="member-desc">
+                a person that has helped me so much these days,
+                they arent reading this most prob, but i still wanted to say thanks -n4c
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
